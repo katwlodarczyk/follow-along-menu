8bd2cf2bb2e3e1d54db3e8f957309cfbbbbab993
Kat Wlodarczyk <katbwlodarczyk@gmail.com>
2023-02-07 10:37:27 +0000

    added follow-along background + arrow

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const menuItems = document.querySelectorAll("nav ul li");
+const menuBackground = document.querySelector(".menu-background");
+
 menuItems.forEach((menuItem) =>
   menuItem.addEventListener("mouseenter", handleEnter)
 );
@@ -11,13 +13,24 @@ function handleEnter() {
 
   menu.classList.add("menu-enter");
   setTimeout(() => menu.classList.add("menu-enter-active"), 50);
+
+  const menuCoords = menu.getBoundingClientRect();
+  menuBackground.classList.add("open");
+
+  menuBackground.style.setProperty(
+    "top",
+    `${menuCoords.top + window.scrollY}px`
+  );
+  menuBackground.style.setProperty("left", `${menuCoords.left}px`);
+  menuBackground.style.setProperty("height", `${menuCoords.height}px`);
+  menuBackground.style.setProperty("width", `${menuCoords.width}px`);
 }
 
 function handleLeave() {
   const menu = this.querySelector(".menu");
 
   menu.classList.remove("menu-enter", "menu-enter-active");
-
+  menuBackground.classList.remove("open");
   menu.classList.add("menu-leave");
   setTimeout(() => menu.classList.add("menu-leave-active"), 50);
 
